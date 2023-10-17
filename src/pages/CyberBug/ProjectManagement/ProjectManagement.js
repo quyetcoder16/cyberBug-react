72e35f6dff0071267549e65de94953c6b9805b1a
dxquyet16 <quyetcoder16@gmail.com>
2023-10-17 14:18:50 +0700

    error record don't have data

--- src/pages/CyberBug/ProjectManagement/ProjectManagement.js
+++ src/pages/CyberBug/ProjectManagement/ProjectManagement.js
@@ -1,115 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import { Table, Tag, Space, Button, Avatar, Popconfirm, message, Popover, AutoComplete } from 'antd';
-// import ReactHtmlParser from "react-html-parser";
-import { FormOutlined, DeleteOutlined, CloseSquareOutlined } from '@ant-design/icons'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { AutoComplete, Popover, Avatar, Button, Space, Table, Tag } from 'antd';
+import { FormOutlined, DeleteOutlined } from '@ant-design/icons'
+import ReactHtmlParser from 'html-react-parser';
+import { useDispatch, useSelector } from 'react-redux';
+import { GET_LIST_PROJECT_SAGA } from '../../../redux/types/cyberBugConstant/CyberBugConstants';
 import FormEditProject from '../../../components/Form/FormEditProject';
+import { message, Popconfirm } from 'antd';
 
+export default function ProjectManagement() {
 
-
-export default function ProjectManagement(props) {
-    //Lấy dữ liệu từ reducer về component
-    const projectList = useSelector(state => state.ProjectCyberBugsReducer.projectList);
-
+    const { projectList } = useSelector(state => state.ProjectCyberBugsReducer);
     const { userSearch } = useSelector(state => state.UserLoginCyberBugReducer);
-
+    // console.log(userSearch);
     const [value, setValue] = useState('');
-
-
-    //Sử dụng useDispatch để gọi action
     const dispatch = useDispatch();
-    const [state, setState] = useState({
-        filteredInfo: null,
-        sortedInfo: null,
-    });
-
     useEffect(() => {
-        dispatch({ type: 'GET_LIST_PROJECT_SAGA' })
-    }, [])
+        dispatch({
+            type: GET_LIST_PROJECT_SAGA
+        })
+    }, []);
 
+    const [filteredInfo, setFilteredInfo] = useState({});
+    const [sortedInfo, setSortedInfo] = useState({});
     const handleChange = (pagination, filters, sorter) => {
         console.log('Various parameters', pagination, filters, sorter);
-        setState({
-            filteredInfo: filters,
-            sortedInfo: sorter,
-        });
+        setFilteredInfo(filters);
+        setSortedInfo(sorter);
     };
-
     const clearFilters = () => {
-        setState({ filteredInfo: null });
+        setFilteredInfo({});
     };
-
     const clearAll = () => {
-        setState({
-            filteredInfo: null,
-            sortedInfo: null,
-        });
+        setFilteredInfo({});
+        setSortedInfo({});
     };
-
     const setAgeSort = () => {
-        setState({
-            sortedInfo: {
-                order: 'descend',
-                columnKey: 'age',
-            },
+        setSortedInfo({
+            order: 'descend',
+            columnKey: 'age',
         });
     };
 
-    let { sortedInfo, filteredInfo } = state;
-    sortedInfo = sortedInfo || {};
-    filteredInfo = filteredInfo || {};
+
     const columns = [
         {
             title: 'id',
             dataIndex: 'id',
             key: 'id',
-            sorter: (item2, item1) => {
-                return item2.id - item1.id;
-            },
-            sortDirections: ['descend'],
-
         },
         {
-            title: 'projectName',
+            title: 'Project Name',
             dataIndex: 'projectName',
-            key: 'projectName',
-            sorter: (item2, item1) => {
-                let projectName1 = item1.projectName?.trim().toLowerCase();
-                let projectName2 = item2.projectName?.trim().toLowerCase();
-                if (projectName2 < projectName1) {
-                    return -1;
-                }
-                return 1;
-            },
-
-
+            key: 'projectName'
         },
         // {
         //     title: 'description',
         //     dataIndex: 'description',
         //     key: 'description',
         //     render: (text, record, index) => {
-        //         let contentJSX = ReactHtmlParser(text);
-
+        //         const jsx = ReactHtmlParser(text);
         //         return <div>
-        //             {contentJSX}
+        //             {jsx}
         //         </div>
         //     }
         // },
-
         {
             title: 'category',
             dataIndex: 'categoryName',
-            key: 'categoryName',
-            sorter: (item2, item1) => {
-                let categoryName1 = item1.categoryName?.trim().toLowerCase();
-                let categoryName2 = item2.categoryName?.trim().toLowerCase();
-                if (categoryName2 < categoryName1) {
-                    return -1;
-                }
-                return 1;
-            },
-
+            key: 'categoryName'
         },
         {
             title: 'creator',
@@ -117,112 +75,95 @@ export default function ProjectManagement(props) {
             key: 'creator',
             render: (text, record, index) => {
                 return <Tag color="green">{record.creator?.name}</Tag>
-            },
-            sorter: (item2, item1) => {
-                let creator1 = item1.creator?.name.trim().toLowerCase();
-                let creator2 = item2.creator?.name.trim().toLowerCase();
-                if (creator2 < creator1) {
-                    return -1;
-                }
-                return 1;
-            },
-
+            }
         },
         {
             title: 'members',
             key: 'members',
             render: (text, record, index) => {
+                // console.log(record);
+                // console.log(record);
+                console.log(record.members);
                 return <div>
-                    {record.members?.slice(0, 3).map((member, index) => {
-                        return (
-                            <Popover key={index} placement="top" title="members" content={() => {
-                                return <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th>Id</th>
-                                            <th>avatar</th>
-                                            <th>name</th>
-                                            <th></th>
+                    {record.members?.splice(0, 3).map((member, index) => {
+                        return <Popover key={index} placement='top' content={() => {
+                            return (<table className='table'>
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>avatar</th>
+                                        <th>name</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {record.members?.map((item, index) => {
+                                        console.log(index);
+                                        return <tr key={index}>
+                                            <td>{item.userId}</td>
+                                            <td><img src={item.avatar} width="30" height="30" style={{ borderRadius: '15px' }} alt={index} /></td>
+                                            <td>{item.name}</td>
+                                            <td>
+                                                <button onClick={() => {
+                                                    dispatch({
+                                                        type: 'REMOVE_USER_PROJECT_API',
+                                                        userProject: {
+                                                            userId: item.userId,
+                                                            projectId: record.id
+                                                        }
+                                                    })
+
+                                                }} className="btn btn-danger" style={{ borderRadius: '50%' }}>X</button>
+                                            </td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        {record.members?.map((item, index) => {
-                                            return <tr key={index}>
-                                                <td>{item.userId}</td>
-                                                <td><img src={item.avatar} width="30" height="30" style={{ borderRadius: '15px' }} /></td>
-                                                <td>{item.name}</td>
-                                                <td>
-                                                    <button onClick={() => {
-                                                        dispatch({
-                                                            type: 'REMOVE_USER_PROJECT_API',
-                                                            userProject: {
-                                                                userId: item.userId,
-                                                                projectId: record.id
-                                                            }
-                                                        })
-
-                                                    }} className="btn btn-danger" style={{ borderRadius: '50%' }}>X</button>
-                                                </td>
-                                            </tr>
-                                        })}
-                                    </tbody>
-                                </table>
-                            }}>
-                                <Avatar key={index} src={member.avatar} />
-                            </Popover>
-                        )
+                                    })}
+                                </tbody>
+                            </table>)
+                        }}>
+                            <Avatar key={index} src={member.avatar} />
+                        </Popover>
                     })}
-
                     {record.members?.length > 3 ? <Avatar>...</Avatar> : ''}
-
-                    <Popover placement="rightTop" title={"Add user"} content={() => {
+                    <Popover placement="topLeft" title="Add user" content={() => {
                         return <AutoComplete
-
                             options={userSearch?.map((user, index) => {
                                 return { label: user.name, value: user.userId.toString() }
                             })}
-
                             value={value}
-
                             onChange={(text) => {
+                                console.log(text);
                                 setValue(text);
                             }}
-
                             onSelect={(valueSelect, option) => {
-                                //set giá trị của hộp thọa = option.label
-                                setValue(option.label);
-                                //Gọi api gửi về backend
+                                // console.log(record);
                                 dispatch({
                                     type: 'ADD_USER_PROJECT_API',
                                     userProject: {
                                         "projectId": record.id,
-                                        "userId": valueSelect
+                                        "userId": Number(valueSelect)
                                     }
                                 })
-
-
                             }}
-                            style={{ width: '100%' }} onSearch={(value) => {
+                            onSearch={(value) => {
                                 dispatch({
                                     type: 'GET_USER_API',
                                     keyWord: value
                                 })
-
-                            }} />
+                            }}
+                            style={{ width: '100%' }
+                            } />
                     }} trigger="click">
                         <Button style={{ borderRadius: '50%' }}>+</Button>
                     </Popover>
                 </div>
             }
-
         },
-
         {
             title: 'Action',
             dataIndex: '',
             key: 'x',
             render: (text, record, index) => {
-                return <div>
+                return (<div>
                     <button className="btn mr-2 btn-primary" onClick={() => {
                         const action = {
                             type: 'OPEN_FORM_EDIT_PROJECT',
@@ -231,7 +172,7 @@ export default function ProjectManagement(props) {
 
                         //dispatch lên reducer nội dung drawer
                         dispatch(action);
-                        //dispatch dữ liệu dòng hiện tai lên reducer
+
                         const actionEditProject = {
                             type: 'EDIT_PROJECT',
                             projectEditModel: record
@@ -241,11 +182,15 @@ export default function ProjectManagement(props) {
                         <FormOutlined style={{ fontSize: 17 }} />
                     </button>
                     <Popconfirm
-                        title="Are you sure to delete this project?"
+                        title="Delete the task"
+                        description="Are you sure to delete this task?"
                         onConfirm={() => {
-                            dispatch({ type: 'DELETE_PROJECT_SAGA', idProject: record.id })
+                            dispatch({
+                                type: 'DELETE_PROJECT_SAGA',
+                                idProject: record.id,
+                            })
                         }}
-
+                        // onCancel={cancel}
                         okText="Yes"
                         cancelText="No"
                     >
@@ -254,19 +199,26 @@ export default function ProjectManagement(props) {
                         </button>
                     </Popconfirm>
 
-                </div>
-            },
+                </div>);
+            }
         }
     ];
+
     return (
-        <div className="container-fluid m-5">
+        <div className='container-fluid mt-5'>
             <h3>Project management</h3>
-            <Space style={{ marginBottom: 16 }}>
-                <Button onClick={setAgeSort}>Sort age</Button>
-                <Button onClick={clearFilters}>Clear filters</Button>
-                <Button onClick={clearAll}>Clear filters and sorters</Button>
-            </Space>
-            <Table columns={columns} rowKey={"id"} dataSource={projectList} onChange={handleChange} />
+            <>
+                <Space
+                    style={{
+                        marginBottom: 16,
+                    }}
+                >
+                    <Button onClick={setAgeSort}>Sort age</Button>
+                    <Button onClick={clearFilters}>Clear filters</Button>
+                    <Button onClick={clearAll}>Clear filters and sorters</Button>
+                </Space>
+                <Table columns={columns} dataSource={projectList} onChange={handleChange} />
+            </>
         </div>
     )
 }
